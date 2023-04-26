428bfea38ebe2c34d5f07200a6190dc7b66ef878
WagnerSantos98 <wagner.jesus@outlook.com>
2023-04-26 18:01:00 -0300

    reserva

---
+++ db/conexao.php
@@ -0,0 +1,10 @@
+<?php
+
+    $servidor = "localhost";
+    $usuario = "root";
+    $senha = "197328640Ti@";
+    $dbname = "db_reserva";
+
+    $con = mysqli_connect($servidor,$usuario,$senha,$dbname);
+    
+?>
